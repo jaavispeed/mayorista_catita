73d1e0e3832b784be9be15fc7120d76b3d58f586
jaavispeed <ja.rodriguezc@duocuc.cl>
2025-03-30 17:46:01 -0300

    Paading

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,30 @@ import Hero from "./components/Hero/Hero"
 import Information from "./components/Information/Information"
 import Testimonials from "./components/Testimonials/Testimonials"
 import Footer from "./components/Footer/Footer"
-
 function App() {
-
   return (
     <div className="overflow-hidden min-h-screen">
-    <Navbar />
-    <Hero />
-    <Information />
-    <Testimonials />
-    <Footer />
+      <Navbar />
+      <hr className="border-t-2 border-gray-300 my-4" />
+      <div className="py-8">
+        <Hero />
+      </div>
+      <hr className="border-t-2 border-gray-300 my-4" />
+      <div className="py-8">
+        <Information />
+      </div>
+      <hr className="border-t-2 border-gray-300 my-4" />
+      <div className="py-8">
+        <Testimonials />
+      </div>
+      <hr className="border-t-2 border-gray-300 my-4" />
+      <div className="py-8">
+        <Footer />
+      </div>
     </div>
   )
 }
+
+
 
 export default App
